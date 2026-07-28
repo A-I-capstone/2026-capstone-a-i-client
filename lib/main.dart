8ea63f1555afd4c299eaf9ad98ddc4f4664b3ed1
100aeri <aeri100@ajou.ac.kr>
2026-07-28 13:21:18 +0900

    refactor: remove secrets and cleanup old code

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'viewmodels/chat_view_model.dart';
-import 'viewmodels/settings/font_size_viewmodel.dart';
-import 'views/chat/chat_view.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: 'assets/config/.env');
   runApp(
+    MultiProvider(providers: [], child: const CapstoneAiApp()),
+    /* // Legacy code
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => ChatViewModel()),
+        ChangeNotifierProvider(
+          create: (_) {
+            final storage = const SecureStorageImpl();
+            final repository = ChatRepository(storage: storage);
+            return ChatViewModel(chatRepository: repository);
+          },
+        ),
         ChangeNotifierProvider(create: (_) => FontSizeViewModel()),
       ],
       child: const CapstoneAiApp(),
     ),
+  );
+  */
   );
 }
 
@@ -21,8 +31,6 @@ class CapstoneAiApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final fontSizeVM = context.watch<FontSizeViewModel>();
-
     return MaterialApp(
       title: 'Capstone AI Client',
       debugShowCheckedModeBanner: false,
@@ -34,13 +42,11 @@ class CapstoneAiApp extends StatelessWidget {
       builder: (context, child) {
         final mediaQuery = MediaQuery.of(context);
         return MediaQuery(
-          data: mediaQuery.copyWith(
-            textScaler: TextScaler.linear(fontSizeVM.currentScale),
-          ),
+          data: mediaQuery.copyWith(),
           child: child ?? const SizedBox.shrink(),
         );
       },
-      home: const ChatView(),
+      //home: const ChatView(),
     );
   }
 }
