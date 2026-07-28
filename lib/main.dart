49871c72f492b4cfec0c0ff9baf2a80f2841a34e
100aeri <aeri100@ajou.ac.kr>
2026-07-28 13:03:26 +0900

    refactor: 새 구조 적용을 위해 기존 legacy 화면 정리

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'viewmodels/chat_view_model.dart';
-import 'viewmodels/settings/font_size_viewmodel.dart';
-import 'views/chat/chat_view.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ChatViewModel()),
-        ChangeNotifierProvider(create: (_) => FontSizeViewModel()),
-      ],
-      child: const CapstoneAiApp(),
-    ),
-  );
+  runApp(MultiProvider(providers: [], child: const CapstoneAiApp()));
 }
 
 class CapstoneAiApp extends StatelessWidget {
@@ -21,8 +10,6 @@ class CapstoneAiApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final fontSizeVM = context.watch<FontSizeViewModel>();
-
     return MaterialApp(
       title: 'Capstone AI Client',
       debugShowCheckedModeBanner: false,
@@ -34,13 +21,11 @@ class CapstoneAiApp extends StatelessWidget {
       builder: (context, child) {
         final mediaQuery = MediaQuery.of(context);
         return MediaQuery(
-          data: mediaQuery.copyWith(
-            textScaler: TextScaler.linear(fontSizeVM.currentScale),
-          ),
+          data: mediaQuery.copyWith(),
           child: child ?? const SizedBox.shrink(),
         );
       },
-      home: const ChatView(),
+      //home: const ChatView(),
     );
   }
 }
